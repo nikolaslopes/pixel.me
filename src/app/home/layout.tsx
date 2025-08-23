b022e09c394cfa038cf173ebe87f3cb08dfe3a07
nikolaslopes <nikolaslopes.dev@gmail.com>
2025-08-22 20:25:00 -0400

    feat: add new metadata

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -15,6 +15,24 @@ const pixelify = Pixelify_Sans({
 export const metadata: Metadata = {
   title: 'pixel.me',
   description: 'Turn your images into pixel art with a click',
+  metadataBase: new URL('https://pixelme-pi.vercel.app/home'),
+
+  openGraph: {
+    title: 'pixel.me',
+    description: 'Turn your images into pixel art with a click',
+    url: '/',
+    siteName: 'pixel.me',
+    images: [
+      {
+        url: 'src/app/favicon.ico',
+        width: 1200,
+        height: 630,
+        alt: 'A promotional image for pixel.me showing pixelated art.',
+      },
+    ],
+    locale: 'src/app/favicon.ico',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
